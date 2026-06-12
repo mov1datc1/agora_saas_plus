a699e0c6e798fddab57fb0ab1fd86fa09b1e3f16
Jonathan Palacios <jonathanpalacios@MacBook-Air-de-Jonathan.local>
2026-06-11 18:41:18 -0600

    fix: await headers() in nextjs 16 to fix vercel build error

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -7,7 +7,8 @@ import { SubscriptionStatus } from '@prisma/client'
 
 export async function POST(req: Request) {
   const body = await req.text()
-  const signature = headers().get('Stripe-Signature') as string
+  const headersList = await headers()
+  const signature = headersList.get('Stripe-Signature') as string
 
   let event: Stripe.Event
 
